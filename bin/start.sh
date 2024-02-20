68044ff77aa490ff7a24ee2781342efe8c05e1c0
Christian Franke <christian.franke@picturepipe.com>
2024-02-20 20:43:43 +0100

    Load config from /conf
    This allows us to mount the configuration in k8s.

--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
-if [ -e "/wg0.conf" ]; then
-	wg-quick up /wg0.conf
+if [ -e "/conf/wg0.conf" ]; then
+	wg-quick up /conf/wg0.conf
 fi
 
-exec su pgbouncer -c 'exec /usr/bin/pgbouncer /pgbouncer.conf'
+exec su pgbouncer -c 'exec /usr/bin/pgbouncer /conf/pgbouncer.conf'
